ce315acc0544d3320f011ee5d92768f59ace646e
Thomas7520 <thomasgerard1406@gmail.com>
2024-03-29 11:50:47 +0100

    Progressing on login

--- src/login/sign_up.py
+++ src/login/sign_up.py
@@ -1,28 +1,54 @@
 import customtkinter
 import os, sys
+from PIL import Image, ImageTk
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from utils import center_window
+import utils
 
     
 def button_callback():
     print("button pressed")
 
 app = customtkinter.CTk()
+
 app.title("Login")
-
-#center(app)
+app.resizable(width=False, height=False)
 window_height = 500
-window_width = 900
+window_width = 1000
 
-center_window(app, window_width, window_height)
+utils.center_window(app, window_width, window_height)
+
+#app.grid_columnconfigure(0, weight=1)
+#app.grid_columnconfigure(1, weight=2)
+
+login_canvas = customtkinter.CTkCanvas(app, width=window_width - 300, height=window_height - 150, background=app["bg"], bd=0, highlightthickness=0)
+
+login_canvas.place(relx=0.5, rely=0.57, anchor=customtkinter.CENTER)
+login_canvas.update()
+
+#utils.round_rectangle(login_canvas, 0,0, login_canvas.winfo_width(),login_canvas.winfo_height(), radius=60, fill="#525252")
+
+"""label_username = customtkinter.CTkLabel(login_canvas, text="Nom d'utilisateur")
+label_password = customtkinter.CTkLabel(login_canvas, text="Mot de passe")
 
 
+#label_username.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+#label_username.pack()
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.grid(row=0, column=0, padx=20, pady=20)
+button_login = customtkinter.CTkButton(app, text="Se connecter", command=button_callback)
+#button_login.grid(row=1, column=2)"""
+
+chemin_images = os.path.abspath(os.path.join(os.getcwd(), "images"))
+
+
+img = ImageTk.PhotoImage(Image.open(f"{chemin_images}\\background_login.png").resize((login_canvas.winfo_width(), login_canvas.winfo_height())))
+login_canvas.create_image(0,0, anchor=customtkinter.NW, image=img)
+
+
 
 app.mainloop()
+
+
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,30 @@ def center_window(app, window_width, window_height):
     y_cordinate = int((screen_height/2) - (window_height/2))
 
     app.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+    
+    
+def round_rectangle(canvas, x1, y1, x2, y2, radius=25, **kwargs):
+        
+    points = [x1+radius, y1,
+              x1+radius, y1,
+              x2-radius, y1,
+              x2-radius, y1,
+              x2, y1,
+              x2, y1+radius,
+              x2, y1+radius,
+              x2, y2-radius,
+              x2, y2-radius,
+              x2, y2,
+              x2-radius, y2,
+              x2-radius, y2,
+              x1+radius, y2,
+              x1+radius, y2,
+              x1, y2,
+              x1, y2-radius,
+              x1, y2-radius,
+              x1, y1+radius,
+              x1, y1+radius,
+              x1, y1]
+
+    return canvas.create_polygon(points, **kwargs, smooth=True)
+
