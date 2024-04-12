a86efb865e131190f3138dbf55aee62383255bb4
Thomas7520 <thomasgerard1406@gmail.com>
2024-04-12 11:13:59 +0200

    Clean up code and creating csv file if it doesn't exist

--- src/main.py
+++ src/main.py
@@ -4,13 +4,12 @@ import utils
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "login"))
 import sign_up
 
-if __name__ == "__main__":
-    
-    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    database_root = os.path.join(root_path, "database")
-    
-    if not os.path.isdir(database_root):
-        os.mkdir(database_root)
+if __name__ == "__main__":    
+    if not os.path.isdir(utils.database_path):
+        os.mkdir(utils.database_path)
         utils.create_empty_csv()
     
+    if not os.path.exists(utils.database_path / utils.database_name):
+        utils.create_empty_csv()
+        
     sign_up.run()
